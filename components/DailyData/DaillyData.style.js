0b57ac58ee9d785106e873209cf2212b324a206c
Roman Firsov <froma2015@gmail.com>
2024-12-02 16:29:23 +0000

    Improve DaillyData styles.

---
+++ components/DailyData/DaillyData.style.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    marginHorizontal: 'auto'
+  },
+  lineChart: {
+    borderRadius: 25,
+    alignSelf: 'center',
+    paddingBottom: 50
+  }
+});
+
+export default styles;
